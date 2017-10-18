7d7adf63ec3e697794f3855e162ccbd3222df58b
Oleg Konopka <konopka_o@ki-technology.ru>
2017-10-19 02:54:39 +0300

    Add validation logic to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
 	belongs_to :category
+	validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+	validates :body, presence: true, length: { minimum: 50, maximum: 1000 }
+
+	scope :is_moderated, -> { where(moderated: true) }
+	scope :most_popular, -> { self.order("views DESC").limit(5) }
 end
