337ada7e5a33688b285f222004457426b1ad1b39
Oleg Konopka <konopka_o@ki-technology.ru>
2017-10-19 02:58:04 +0300

    Fix routes paths and clean up logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :get_article, only: [:show, :edit, :update, :destroy, :article_popularity_rate]
   def index
-  	@articles = Article.all
+  	@articles = Article.all.order('created_at DESC').is_moderated
   	@article = Article.new
     @category = Category.first
     @categories = Category.all
+    @popular_articles = Article.all.is_moderated.most_popular
   end
+
   def new
     @article = Article.new
     @category = Category.first
@@ -13,26 +16,32 @@ class ArticlesController < ApplicationController
   
   def create
     @article=Article.new(article_params)
-    redirect_to root_path if @article.save		
+    if @article.save
+      redirect_to root_path
+    else
+      render 'new'
+    end 		
   end
 
   def show
-    @article=Article.find(params[:id])
     @category = Category.find(@article.category_id)
+    article_popularity_rate
   end
+
   def edit
-    @article=Article.find(params[:id])
     @category = Category.find(@article.category_id)
     @categories = Category.all
   end
 
   def update
-    @article=Article.find(params[:id])
-    redirect_to root_path if @article.update_attributes(article_params)
+    if @article.update_attributes(article_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    @article=Article.find(params[:id])
     @category = Category.find(@article.category_id)
     @article.destroy
     redirect_to root_path
@@ -40,7 +49,15 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :body, :user_id, :moderated, :category_id)
+    params.require(:article).permit(:title, :body, :user_id, :moderated, :category_id, :views)
   end
 
+  def get_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_popularity_rate
+    new_view_count = @article.views.to_i + 1;
+    @article.update(views: new_view_count)
+  end
 end
