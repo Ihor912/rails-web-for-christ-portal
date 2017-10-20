a846cf990ca661b271a80d388e3de9b34c041572
Ihor Khomiak <igor.homyak@gmail.com>
2017-10-20 16:11:45 +0300

    fixes related to flash messages, user/article functionality

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,10 @@
+module ArticlesHelper
+    def get_author_name_from_article(article)
+      unless article.user_id
+        return ''
+      end
+  
+      user = User.find(article.user_id)
+      user.email.split('@')[0]
+    end
+end
