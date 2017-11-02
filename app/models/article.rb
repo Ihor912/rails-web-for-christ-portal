b48536845da316125ccd457373490520133553bd
Ihor Khomiak <igor.homyak@gmail.com>
2017-11-02 11:52:49 +0200

    styling show(details) page and add small fixes to different functionalities

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
 	has_and_belongs_to_many :tags
 	
 	validates :title, presence: true, length: { minimum: 5, maximum: 50 }
-	validates :body, presence: true, length: { minimum: 50, maximum: 1000 }
+	validates :body, presence: true, length: { minimum: 50, maximum: 10000 }
 	validates :link_name, presence: true, length: { maximum: 25 }, uniqueness: true
 
 	scope :most_popular, -> { self.order("views DESC").limit(4) }
