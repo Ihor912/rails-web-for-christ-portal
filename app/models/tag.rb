6d94d101e4e6b972661ed3730386d64211a46812
Oleg Konopka <konopka_o@ki-technology.ru>
2017-11-05 01:10:01 +0200

    Add tags CRUD functionality

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
 	has_and_belongs_to_many :articles
+
+	validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+	validates :link_name, presence: true, length: { maximum: 25 }, uniqueness: true
+
 end
